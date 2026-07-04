4b0e104260657cc842d26c0f7d197b8e5fbfb02d
NilsGi <38689849+NilsGi@users.noreply.github.com>
2026-07-04 23:32:16 +0200

    Delete tools/calendar/config.js

--- tools/calendar/config.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { SUPABASE_ANON_KEY, SUPABASE_URL } from "./config.js";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
